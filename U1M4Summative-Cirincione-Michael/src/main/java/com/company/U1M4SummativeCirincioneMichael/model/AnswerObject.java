1014ad18a630cb24932f3ed6bbb57d64f7e2bb65
mjc9864 <mjc9864@gmail.com>
2019-07-01 06:33:54 -0400

    Add files via upload

---
+++ U1M4Summative-Cirincione-Michael/src/main/java/com/company/U1M4SummativeCirincioneMichael/model/AnswerObject.java
@@ -0,0 +1,27 @@
+package com.company.U1M4SummativeCirincioneMichael.model;
+
+public class AnswerObject {
+
+    private String question;
+    private String answer;
+
+    public AnswerObject() {
+    }
+
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
